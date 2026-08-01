0b53115aa1e043b9d0d89a1baee277ab619648ab
Dskemz <denis.masquet@gmail.com>
2026-08-01 23:26:29 +0200

    test color texte vs bandeau

--- src/components/projects/Withings/HeroSection.tsx
+++ src/components/projects/Withings/HeroSection.tsx
@@ -77,7 +77,7 @@ export default function HeroSection() {
         className="relative z-10 h-full w-full flex flex-col items-center justify-center px-6 text-center md:items-start md:text-left md:px-16 lg:px-24"
       >
         <div className="mb-6 tracking-widest">
-          <p className="text-sm md:text-base font-light text-#7a8087 uppercase drop-shadow-sm">
+          <p className="text-sm md:text-base font-light text-[#7a8087] uppercase drop-shadow-sm">
             Withings, Modélisation 3D &amp; Horlogerie connectée
           </p>
         </div>
