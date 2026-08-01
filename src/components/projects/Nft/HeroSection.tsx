d4f60c0a421e127d8feb5a061f9e261967252b60
Dskemz <denis.masquet@gmail.com>
2026-08-01 23:55:30 +0200

    white

--- src/components/projects/Nft/HeroSection.tsx
+++ src/components/projects/Nft/HeroSection.tsx
@@ -45,7 +45,7 @@ export default function HeroSection() {
   return (
     <section
       ref={containerRef}
-      className="relative w-full h-screen bg-gradient-to-br from-[#4a2c5c] to-[#2d1b3a] overflow-hidden"
+      className="relative w-full h-screen bg-gradient-to-br from-[#8128a1] to-[#6e228a] overflow-hidden"
     >
       <div
         ref={heroImageRef}
